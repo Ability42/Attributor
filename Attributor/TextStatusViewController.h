6292d986e1fd40b063e9327e639a9f2bc73ac1f0
Stepan Paholyk <stepanpaholyk@gmail.com>
2016-10-04 21:02:59 +0300

    Initial_2

---
+++ Attributor/TextStatusViewController.h
@@ -0,0 +1,13 @@
+//
+//  TextStatusViewController.h
+//  Attributor
+//
+//  Created by Stepan Paholyk on 9/20/16.
+//  Copyright © 2016 Stepan Paholyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TextStatusViewController : UIViewController
+
+@end
